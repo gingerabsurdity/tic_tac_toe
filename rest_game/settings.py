9adcbb5d82b357f1a77a6aa77001fa7031ca75c0
antonina <gingerabsurdity@gmail.com>
2017-11-24 04:30:38 +0300

    add PostgreSQL connection

--- rest_game/settings.py
+++ rest_game/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'rest_game.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'django_db',
+    'USER' : 'user_name',
+    'PASSWORD' : 'password',
+    'HOST' : '127.0.0.1',
+    'PORT' : '5432',
     }
 }
 
